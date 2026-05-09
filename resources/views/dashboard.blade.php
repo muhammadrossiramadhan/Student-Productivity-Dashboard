fdc893e5e5389897436a9b7850dd2be131320102
muhammadrossiramadhan <rossilesson@gmail.com>
2026-05-10 02:48:58 +0700

    ini harusnya ada sih

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -18,7 +18,7 @@
                 <i class="fas fa-graduation-cap"></i> STUDENT.IO
             </a>
         </div>
-        <div class="hamburger" id="hamburgerMenu">
+        <div class="hamburger" id="hamburgerMenu">// hamburger menu dari yoshua
             <i class="fas fa-bars"></i>
         </div>
     </div>
